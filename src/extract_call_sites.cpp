1d2deb845e72f20b2b634bca32ce1de75db94322
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-06-22 21:00:31 -0400

    update

--- src/extract_call_sites.cpp
+++ src/extract_call_sites.cpp
@@ -19,11 +19,11 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <bfd.h>
-#define N 8
+#define N 1
 
 using namespace std;
 
-void thread_function(vector<pair<long, long>> func_name, unordered_map<long, call_site_info> &call_sites, string new_target_binary, ocolos_env* ocolos_environ){
+void thread_function(vector<pair<long, long>> func_addr, unordered_map<long, call_site_info> &call_sites, string new_target_binary, ocolos_env* ocolos_environ){
    int fd = open(new_target_binary.c_str(), O_RDONLY);
    if (fd < 0) {
       perror("open");
@@ -44,11 +44,9 @@ void thread_function(vector<pair<long, long>> func_name, unordered_map<long, cal
    size_t shstrndx;
    elf_getshdrstrndx(e, &shstrndx);
 
-   for (unsigned i=0; i<func_name.size(); i++){
-      FILE *fp;
-      char path[1000];
-      string start_addr = convert_long_2_hex_string(func_name[i].first);
-      string stop_addr = convert_long_2_hex_string(func_name[i].first + func_name[i].second);
+   for (unsigned i=0; i<func_addr.size(); i++){
+      string start_addr = convert_long_2_hex_string(func_addr[i].first);
+      string stop_addr = convert_long_2_hex_string(func_addr[i].first + func_addr[i].second);
 
       uint64_t start_addr_ = convert_str_2_long(start_addr);
       uint64_t stop_addr_ = convert_str_2_long(stop_addr);
@@ -64,8 +62,10 @@ void thread_function(vector<pair<long, long>> func_name, unordered_map<long, cal
             uint64_t section_offset = shdr.sh_offset;
 
             if (start_addr_ < section_addr || stop_addr_ > section_addr + shdr.sh_size) {
-               fprintf(stderr, "start addr: 0x%lx, stop addr: 0x%lx\n", start_addr_, stop_addr_);
-               fprintf(stderr, "Address range not in .text section\n");
+               fprintf(stderr, "text section starting addr: 0x%lx, stop addr: 0x%lx\n", 
+                  section_addr, section_addr + shdr.sh_size);
+               fprintf(stderr, "function start addr: 0x%lx, stop addr: 0x%lx\n", start_addr_, stop_addr_);
+               fprintf(stderr, "function address range not in .text section\n");
                break;
             }
 
@@ -87,39 +87,40 @@ void thread_function(vector<pair<long, long>> func_name, unordered_map<long, cal
             for (size_t j = 0; j < count; j++) {
                //printf("@@@ 0x%" PRIx64 ": %s %s\n", insn[j].address, insn[j].mnemonic, insn[j].op_str);
                if (insn[j].id == X86_INS_CALL && insn[j].detail != NULL) {
-               printf("0x%" PRIx64 ": %s %s\n",
-                      insn[j].address,
-                      insn[j].mnemonic,
-                      insn[j].op_str);
  
-               detail = insn[j].detail;
-               cs_x86 *x86 = &detail->x86;
+                  detail = insn[j].detail;
+                  cs_x86 *x86 = &detail->x86;
 
-               // We only care about direct call instructions
-               if (x86->op_count > 0 && x86->operands[0].type == X86_OP_IMM) {
-                  call_site_info call_info;
-                  call_info.addr = insn[j].address;
-                  call_info.belonged_func = func_name[i].first;
+                  // We only care about direct call instructions
+                  if (x86->op_count > 0 && x86->operands[0].type == X86_OP_IMM) {
+                     printf("0x%" PRIx64 ": %s %s\n",
+                        insn[j].address,
+                        insn[j].mnemonic,
+                        insn[j].op_str);
 
-                  call_info.next_addr = insn[j+1].address;
-                  uint64_t target = x86->operands[0].imm;
-                  call_info.target_func_addr = target;
+                     call_site_info call_info;
+                     call_info.addr = insn[j].address;
+                     call_info.belonged_func = func_addr[i].first;
 
-                  size_t inst_size = insn[j].size;
-                  uint8_t *bytes = insn[j].bytes;
-                  if (inst_size != 5) {
-                     fprintf(stderr, "The size of call instruction is not 5 \n");
+                     call_info.next_addr = insn[j+1].address;
+                     uint64_t target = x86->operands[0].imm;
+                     call_info.target_func_addr = target;
+
+                     size_t inst_size = insn[j].size;
+                     uint8_t *bytes = insn[j].bytes;
+                     if (inst_size != 5) {
+                        fprintf(stderr, "The size of call instruction is not 5 \n");
+                     }
+                     // Allocate array and copy bytes
+                     memcpy(call_info.machine_code, bytes, inst_size);
+                     call_sites[call_info.addr]=call_info;
                   }
-                  // Allocate array and copy bytes
-                  memcpy(call_info.machine_code, bytes, inst_size);
-                  call_sites[call_info.addr]=call_info;
                }
-             }
-           }
+            }
 
-           cs_free(insn, count);
-           cs_close(&handle);
-           break;
+            cs_free(insn, count);
+            cs_close(&handle);
+            break;
          }
       }
    }
@@ -207,36 +208,38 @@ int main(){
    system(command.c_str());
 
    // get all function: starting address, length
-   vector<pair<long, long>> func_name = extract_function_ranges(new_target_binary);
+   vector<pair<long, long>> func_addr = extract_function_ranges(new_target_binary);
 
-   int size =(int)( func_name.size()/N);
-	
-   vector<vector<pair<long, long>>> func_names;
+   int size =(int)( func_addr.size()/N);
+
+   // split function starting addresses into different 
+   // vectors for parallel computing
+   vector<vector<pair<long, long>>> func_addr_array;
    for (int i=0; i<N; i++){
       if (i!=(N-1)){
-         vector<pair<long, long>>::const_iterator first = func_name.begin()+i*size;
-         vector<pair<long, long>>::const_iterator last = func_name.begin()+(i+1)*size;
+         vector<pair<long, long>>::const_iterator first = func_addr.begin()+i*size;
+         vector<pair<long, long>>::const_iterator last = func_addr.begin()+(i+1)*size;
          vector<pair<long, long>> new_str(first, last);
-         func_names.push_back(new_str);
+         func_addr_array.push_back(new_str);
       }
       else{
-         vector<pair<long, long>>::const_iterator first = func_name.begin()+i*size;
-         vector<pair<long, long>>::const_iterator last = func_name.end();
+         vector<pair<long, long>>::const_iterator first = func_addr.begin()+i*size;
+         vector<pair<long, long>>::const_iterator last = func_addr.end();
          vector<pair<long, long>> new_str(first, last);
-         func_names.push_back(new_str);
+         func_addr_array.push_back(new_str);
       }
    }
    unordered_map<long, call_site_info> call_sites_array[N];
 	
    vector<thread> threads;	
    for (int i=0; i<N; i++){
-      thread t(thread_function, func_names[i], ref(call_sites_array[i]), new_target_binary, &ocolos_environ);		
+      thread t(thread_function, func_addr_array[i], ref(call_sites_array[i]), new_target_binary, &ocolos_environ);		
       threads.push_back(std::move(t));
    }	
 
    for (int i=0; i<N; i++){
       threads[i].join();
-      printf("the size of call_sites = %lu\n", call_sites_array[i].size());
+      printf("@@@ the size of call_sites = %lu\n", call_sites_array[i].size());
    }
 
    unordered_map<long, call_site_info> call_sites;
